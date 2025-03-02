b547cd2bae03e888322ae2c3708180145980c73a
John <john1812008@gmail.com>
2025-03-02 22:46:33 +0200

    Finished Ten Class in model.card.standard

--- src/model/card/standard/Ten.java
+++ src/model/card/standard/Ten.java
@@ -1,5 +1,10 @@
 package model.card.standard;
 
-public class Ten extends Standard {
+import engine.GameManager;
+import engine.board.BoardManager;
 
+public class Ten extends Standard {
+    public Ten(String name, String description, Suit suit, BoardManager boardManager, GameManager gameManager){
+        super(name, description, 10, suit, boardManager, gameManager);
+    }
 }
