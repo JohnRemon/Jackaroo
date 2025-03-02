28448763a51939748be91ef92cdfe0669211e244
John <john1812008@gmail.com>
2025-03-02 20:10:32 +0200

    Created the SafeZone Class in engine.board package and final keyword for instance variables

---
+++ src/engine/board/SafeZone.java
@@ -0,0 +1,18 @@
+package engine.board;
+
+import model.Colour;
+
+import java.util.ArrayList;
+
+public class SafeZone {
+    private final Colour colour;
+    private final ArrayList<Cell> cells;
+
+    public SafeZone(Colour colour) {
+        this.colour = colour;
+        this.cells = new ArrayList<>();
+        for(int i = 0; i < 4; i++) {
+            cells.add(new Cell(CellType.SAFE));
+        }
+    }
+}
