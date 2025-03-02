4cd6c4d28b3a399cdef44a8c464918f01465cc4b
AnkhRamses <ankh.tech.gaming@gmail.com>
2025-03-02 23:34:37 +0200

    pull request test

--- src/model/card/wild/Saver.java
+++ src/model/card/wild/Saver.java
@@ -7,5 +7,5 @@ public class Saver extends Wild {
     public Saver(String name, String description, BoardManager boardManager, GameManager gameManager){
         super(name, description, boardManager, gameManager);
     }
-
+    //testing pull request
 }
