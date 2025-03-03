9440027dc5901356a237e40d9d19177017d5f6a4
John <john1812008@gmail.com>
2025-03-03 12:18:31 +0200

    Created the Board Class in engine.board package, however the se the track with correct type of cells task is still not completed

--- src/engine/board/Cell.java
+++ src/engine/board/Cell.java
@@ -12,4 +12,26 @@ public class Cell {
         this.cellType = cellType;
         this.trap = false;
     }
+
+    //getter and setter for the marble
+    public Marble getMarble() {
+        return marble;
+    }
+    public void setMarble(Marble marble) {
+        this.marble = marble;
+    }
+    //getter and setter for the cellType
+    public CellType getCellType() {
+        return cellType;
+    }
+    public void setCellType(CellType cellType) {
+        this.cellType = cellType;
+    }
+    //getter and setter for the trap
+    public boolean isTrap() {
+        return trap;
+    }
+    public void setTrap(boolean trap) {
+        this.trap = trap;
+    }
 }
