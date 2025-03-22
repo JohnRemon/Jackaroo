4523338d7827c4be62a34a22d3aca035da72014e
John <john1812008@gmail.com>
2025-03-22 20:05:57 +0200

    Added the Required Methods to the GameManger Interface

--- src/engine/GameManager.java
+++ src/engine/GameManager.java
@@ -1,7 +1,19 @@
 package engine;
 
 import engine.board.Board;
+import exception.CannotDiscardException;
+import exception.CannotFieldException;
+import exception.IllegalDestroyException;
+import model.Colour;
+import model.player.Marble;
 
 public interface GameManager {
+    void sendHome(Marble marble);
+    void fieldMarble() throws CannotFieldException, IllegalDestroyException;
+    void discardCard(Colour colour) throws CannotDiscardException;
+    void discardCard() throws CannotDiscardException;
+    Colour getActivePlayerColour();
+    Colour getNextPlayerColour();
+
 
 }
